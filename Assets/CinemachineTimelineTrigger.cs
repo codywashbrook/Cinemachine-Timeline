971b420c0290be9861accfb6b03fd8035ac3ae7b
codywashbrook <codywashbrook@hotmail.com>
2022-12-09 01:24:31 -0400

    new fps and script used

--- Assets/CinemachineTimelineTrigger.cs
+++ Assets/CinemachineTimelineTrigger.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class TimelineTrigger : MonoBehaviour
+public class CinemachineTimelineTrigger : MonoBehaviour
 {
     public PlayableDirector timeline;
-
+    
     public GameObject player;
 
     public bool OneTimeTrigger = false; // true = this trigger will self destruct after one use.
@@ -14,10 +14,10 @@ public class TimelineTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
-        {
+        {                     
             timeline.Play();
-            player.GetComponent<FirstPersonController_Sam>().enabled = false;
-
+            player.GetComponent<FirstPersonController_Sam>().enabled = false;  
+            
             // TODO: Need to add logic to listen for cinemachine event complete and reactivate player control
 
             // only use if you want to have an event that only triggers once
